9b589b89a56a8f05214d5cf4020b0272b3c84442
Wissle <jongsome@gmail.com>
2024-10-02 00:44:54 +0900

    feat: Add GLFW based window for Pizza Preview Window.

--- pizza-preview-window/app/Main.hs
+++ pizza-preview-window/app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Control.Monad
+
+import Data.Foldable
+import Data.IORef
+import Data.Function
+
+import qualified Graphics.UI.GLFW as GLFW
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    initSucc <- GLFW.init
+    when initSucc $ do
+        keepAlive <- newIORef True
+        mwin <- GLFW.createWindow 400 400 "Pizza Preview" Nothing Nothing
+        for_ mwin $ \win -> do
+            GLFW.setWindowCloseCallback win $ Just (\_ -> writeIORef keepAlive False)
+            GLFW.showWindow win
+
+            fix $ \recur -> do
+                a <- readIORef keepAlive
+                when a (GLFW.waitEvents >> recur)
+        GLFW.terminate
