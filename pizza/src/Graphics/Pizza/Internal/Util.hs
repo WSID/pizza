e98ecf47b7f1ff4ba49efb8c0127a8b75af67a9f
Wissle <jongsome@gmail.com>
2024-12-04 00:53:10 +0900

    feat: Added scalar polynomial.
    This will be used for b-spline construction.

--- pizza/src/Graphics/Pizza/Internal/Util.hs
+++ pizza/src/Graphics/Pizza/Internal/Util.hs
@@ -5,7 +5,7 @@ import Linear
 
 {- Dense Vector Polynomial for scalar input
  -}
-newtype VPoly = VPoly { factors :: [V2 Float] }
+newtype VPoly = VPoly { factors :: [V2 Float] } deriving (Eq, Show, Read)
 
 runVPoly :: Float -> VPoly -> V2 Float
 runVPoly x (VPoly f) = sum $ zipWith (^*) f $ iterate (* x) 1
@@ -37,4 +37,45 @@ lerpVPoly a b c = addVPoly (smulVPoly (1 - a) b) (smulVPoly a c)
 -- Constant part is dropped
 derivVPoly :: VPoly -> VPoly
 derivVPoly (VPoly []) = VPoly []
-derivVPoly (VPoly (_: fs)) = VPoly $ zipWith (*^) [1 ..] fs 
+derivVPoly (VPoly (_: fs)) = VPoly $ zipWith (*^) [1 ..] fs
+
+
+
+newtype Poly = Poly { polyFactors :: [Float] } deriving (Eq, Show, Read)
+
+runPoly :: Float -> Poly -> Float
+runPoly x (Poly f) = sum $ zipWith (*) f $ iterate (* x) 1
+
+polyFromFunc :: (Poly -> Poly) -> Poly
+polyFromFunc f = f idPoly
+
+smulPoly :: Float -> Poly -> Poly
+smulPoly a (Poly b) = Poly (fmap (a *) b)
+
+polyCons :: Float -> Poly -> Poly
+polyCons a (Poly b) = Poly (a : b)
+
+instance Num Poly where
+    Poly a + Poly b = Poly (go a b)
+      where
+        go [] bs = bs
+        go as [] = as
+        go (ah: as) (bh: bs) = ah + bh : go as bs
+
+    Poly a * b = go a b
+      where
+        go [] _ = Poly []
+        go (ah : as) bp = (smulPoly ah bp) + polyCons 0 (go as bp)
+
+    negate (Poly a) = Poly (fmap negate a)
+
+    abs (Poly a) = Poly $ fmap abs a
+    signum (Poly a) = Poly $ fmap signum a
+    fromInteger v = Poly [fromInteger v]
+
+idPoly :: Poly
+idPoly = Poly [0, 1]
+
+compPoly :: Poly -> Poly -> Poly
+compPoly (Poly f) a = sum $ zipWith smulPoly f $ iterate (* a) 1
+
