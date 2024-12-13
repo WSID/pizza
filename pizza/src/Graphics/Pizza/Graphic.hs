a530ddf39015b29968c9425ff1bcc678c4fa3fcf
Wissle <jongsome@gmail.com>
2024-12-13 22:51:24 +0900

    refact: Separated Curve from PathPart.
    Preparation for member addition.

--- pizza/src/Graphics/Pizza/Graphic.hs
+++ pizza/src/Graphics/Pizza/Graphic.hs
@@ -7,6 +7,8 @@ import Control.Applicative
 -- linear
 import Linear
 
+import Graphics.Pizza.Graphic.Curve
+import qualified Graphics.Pizza.Graphic.Curve as Curve
 import Graphics.Pizza.Internal.Geometry
 import Graphics.Pizza.Internal.Util
 import Control.Arrow (ArrowChoice(right))
@@ -23,10 +25,7 @@ data AABB = AABB !(V2 Float) !(V2 Float)
 
 data PathPart
     = PathPoint (V2 Float)
-    | PathCurve {
-        pathCurvePos :: Float -> V2 Float,
-        pathCurveDir :: Float -> V2 Float
-    }
+    | PathCurve Curve
 
 newtype Path = Path [PathPart]
 
@@ -56,7 +55,7 @@ pathPartToPoints p = pathPartToPoints' (PathSplitOptions 10 2) p
 
 pathPartToPoints' :: PathSplitOptions -> PathPart -> [V2 Float]
 pathPartToPoints' _ (PathPoint a) = [a]
-pathPartToPoints' opt (PathCurve f _) = [f 0] ++ go 0 (f 0) 1 (f 1) ++ [f 1]
+pathPartToPoints' opt (PathCurve (Curve f _)) = [f 0] ++ go 0 (f 0) 1 (f 1) ++ [f 1]
   where
     go start sv end ev
         | continue = go start sv m mv ++ [mv] ++ go m mv end ev
@@ -79,12 +78,12 @@ rangeDivList n = fmap (\i -> fromIntegral i / nf) [0 .. n]
 -- Curves
 
 bezier :: V2 Float -> [V2 Float] -> V2 Float -> PathPart
-bezier start controls end = polyCurve (bezierPoly start controls end)
+bezier start controls end = PathCurve . fromVPoly $ bezierPoly start controls end
 
 arc :: V2 Float -> Float -> Float -> Float -> PathPart
-arc center radius start end = PathCurve {
-    pathCurvePos = \t -> center + angle (start + (end - start) * t) ^* radius,
-    pathCurveDir = \t -> negate $ perp $ angle (start + (end - start) * t)
+arc center radius start end = PathCurve $ Curve {
+    curvePosition = \t -> center + angle (start + (end - start) * t) ^* radius,
+    curveDirection = \t -> negate $ perp $ angle (start + (end - start) * t)
 }
 
 -- Paths
@@ -106,44 +105,25 @@ bezierPoly start controls end = result
         VPoly bf = subVPoly b a
         result = addVPoly a (VPoly (zero : bf))
 
-
--- base construction
-
-polyCurve :: VPoly -> PathPart
-polyCurve p = PathCurve {
-    pathCurvePos = \t -> runVPoly t p,
-    pathCurveDir = \t -> runVPoly t dp
-}
-  where
-    dp = derivVPoly p
-
-
 -- outline
-
-reversePathPart :: PathPart -> PathPart
-reversePathPart (PathPoint a) = PathPoint a
-reversePathPart (PathCurve pos dir) = PathCurve (\t -> pos (1 - t)) (\t -> negate $ dir (1 - t))
 
 pathPartStart :: PathPart -> V2 Float
 pathPartStart (PathPoint p) = p
-pathPartStart (PathCurve pos _) = pos 0
+pathPartStart (PathCurve (Curve pos _)) = pos 0
 
 pathPartEnd :: PathPart -> V2 Float
 pathPartEnd (PathPoint p) = p
-pathPartEnd (PathCurve pos _) = pos 1
+pathPartEnd (PathCurve (Curve pos _)) = pos 1
 
 stroke :: Float -> Bool -> Path -> [Path]
 stroke _ _ (Path []) = []
 stroke _ _ (Path [PathPoint _]) = []
 
-stroke thickness closed (Path [PathCurve apos adir])
+stroke thickness closed (Path [PathCurve curve])
   | closed      = [ Path [lc], Path [rc] ]
   | otherwise   = [ Path [lc, rc] ]
   where
-    (lpos, ldir) = leftCurve thickness apos adir
-    (rpos, rdir) = rightCurve thickness apos adir
-    lc = PathCurve lpos ldir
-    rc = PathCurve rpos rdir
+    (lc, rc) = leftRightCurve thickness curve
 
 stroke thickness closed (Path (p: q: ps))
   | closed      = [Path lres, Path rres]
@@ -161,15 +141,19 @@ stroke thickness closed (Path (p: q: ps))
         (nextLeft, nextRight) = onIter nextDir cp cps
 
     onIter prevDir (PathPoint a) rest = onPoint a prevDir rest
-    onIter prevDir (PathCurve pos dir) rest =
+    onIter prevDir (PathCurve c) rest =
         (
             [PathPoint al, lc] <> nextLeft,
             nextRight <> [rc, PathPoint ar]
         )
       where
-        (lc, rc) = leftRightCurve thickness pos dir
-        (al, ar) = leftRightDir2 thickness (pos 0) prevDir (dir 0)
-        (nextLeft, nextRight) = onPoint (pos 1) (dir 1) rest
+        apos = curvePosition c 0
+        bpos = curvePosition c 1
+        adir = curveDirection c 0
+        bdir = curveDirection c 1
+        (lc, rc) = leftRightCurve thickness c
+        (al, ar) = leftRightDir2 thickness apos prevDir adir
+        (nextLeft, nextRight) = onPoint bpos bdir rest
 
 
     headPos = pathPartStart p
@@ -187,10 +171,12 @@ stroke thickness closed (Path (p: q: ps))
                   where
                     (lp, rp) = leftRightDir thickness a headDir
                     (nl, nr) = onIter headDir q ps
-                PathCurve pos dir -> ([lc] <> nl, nr <> [rc])
+                PathCurve c -> ([lc] <> nl, nr <> [rc])
                   where
-                    (lc, rc) = leftRightCurve thickness pos dir
-                    (nl, nr) = onPoint (pos 1) (dir 1) (q: ps)
+                    bpos = curvePosition c 1
+                    bdir = curveDirection c 1
+                    (lc, rc) = leftRightCurve thickness c
+                    (nl, nr) = onPoint bpos bdir (q: ps)
 
 
 
@@ -210,19 +196,11 @@ leftRightDir2 thickness p d1 d2 = case l of
     l2 = p + e2
     l = lineIntersect l1 d1 l2 d2
 
-leftCurve :: Float -> (Float -> V2 Float) -> (Float -> V2 Float) -> (Float -> V2 Float, Float -> V2 Float)
-leftCurve thickness pos dir = (lpos, dir)
+leftRightCurve :: Float -> Curve -> (PathPart, PathPart)
+leftRightCurve thickness curve = (
+        PathCurve $ half `alongsideOf` curve,
+        PathCurve $ half `alongsideOf` Curve.reverse curve
+    )
   where
-    lpos t = pos t + (perp (signorm (dir t)) ^* (thickness * 0.5))
+    half = thickness * 0.5
 
-rightCurve :: Float -> (Float -> V2 Float) -> (Float -> V2 Float) -> (Float -> V2 Float, Float -> V2 Float)
-rightCurve thickness pos dir = (rpos, rdir)
-  where
-    rpos t = pos (1 - t) - perp (signorm (dir (1 - t)) ^* (thickness * 0.5))
-    rdir t = dir (1 - t)
-
-leftRightCurve :: Float -> (Float -> V2 Float) -> (Float -> V2 Float) -> (PathPart, PathPart)
-leftRightCurve thickness pos dir = (PathCurve lpos ldir, PathCurve rpos rdir)
-  where
-    (lpos, ldir) = leftCurve thickness pos dir
-    (rpos, rdir) = rightCurve thickness pos dir
---
+++ pizza/src/Graphics/Pizza/Graphic/Curve.hs
@@ -0,0 +1,36 @@
+module Graphics.Pizza.Graphic.Curve where
+
+import Linear
+
+import Graphics.Pizza.Internal.Util
+
+-- | A curve on spatial.
+data Curve = Curve {
+    curvePosition :: Float -> V2 Float,
+    curveDirection :: Float -> V2 Float
+}
+
+reverse :: Curve -> Curve
+reverse curve = Curve {
+    curvePosition = \t -> curvePosition curve (1 - t),
+    curveDirection = \t -> - curveDirection curve (1 - t)
+}
+
+alongsideOf :: Float -> Curve -> Curve
+alongsideOf amount curve = Curve {
+    curvePosition = \t ->
+        curvePosition curve t +
+        (signorm . perp $ curveDirection curve t) ^* amount,
+
+    -- TODO: Direction is incorrect!
+    curveDirection = curveDirection curve
+}
+
+fromVPoly :: VPoly -> Curve
+fromVPoly poly = Curve {
+    curvePosition = \t -> runVPoly t poly,
+    curveDirection = \t -> runVPoly t dp
+}
+  where
+    dp = derivVPoly poly
+
