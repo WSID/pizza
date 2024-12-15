cad93d4e11bd05b29bb43b2118741571a5c38317
Wissle <jongsome@gmail.com>
2024-12-15 20:11:49 +0900

    test: Add base tests for Poly.

--- pizza/tests/Test.hs
+++ pizza/tests/Test.hs
@@ -20,6 +20,7 @@ import Linear
 
 -- pizza
 import Graphics.Pizza
+import Graphics.Pizza.Internal.Util
 import Graphics.Pizza.Internal.Geometry
 
 -- test internal
@@ -28,6 +29,28 @@ import TestRendering
 main :: IO ()
 main = do
     runTestTTAndExit $ TestList [
+            "Poly" ~: TestList [
+                "Run" ~: TestList [
+                    "0" ~: runPoly (Poly [5]) 7 ~?= 5,
+                    "1" ~: runPoly (Poly [3, 1]) 7 ~?= 10,
+                    "2" ~: runPoly (Poly [1, 2, 1]) 7 ~?= 64
+                ],
+                "FromFunc" ~: TestList [
+                    "0" ~: polyFromFunc (const 5) ~?= Poly [5],
+                    "1" ~: polyFromFunc (+ 3) ~?= Poly [3, 1],
+                    "2" ~: polyFromFunc (\x -> x*x + 2 * x + 1) ~?= Poly [1, 2, 1]
+                ],
+                "Multiplicaiton" ~: TestList [
+                    "1 x 2" ~: do
+                        let a = Poly [2, 1]
+                            b = Poly [1, 2, 1]
+                        runPoly (a * b) 3 ~?= runPoly a 3 * runPoly b 3,
+                    "2 x 3" ~: do
+                        let a = Poly [3, 3, 1]
+                            b = Poly [1, 2, 3, 4]
+                        runPoly (a * b) 7 ~?= runPoly a 7 * runPoly b 7
+                ]
+            ],
             "Geometry" ~: TestList [
                 "LineIntersect" ~: TestList [
                     "Parallel" ~: do
