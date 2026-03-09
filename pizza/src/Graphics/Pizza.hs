89ea41527baf775b1779d303fce7bb194c35de45
Wissle <jongsome@gmail.com>
2026-03-09 22:36:10 +0900

    feat: Add renderToList facade function.

--- pizza/src/Graphics/Pizza.hs
+++ pizza/src/Graphics/Pizza.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Graphics.Pizza (
     module Graphics.Pizza.Device.Environment,
     module Graphics.Pizza.Device.Exchange,
@@ -8,8 +10,23 @@ module Graphics.Pizza (
     module Graphics.Pizza.Device.RenderState,
     module Graphics.Pizza.Device.RenderTarget,
     module Graphics.Pizza.Graphic,
-    module Graphics.Pizza.Painting
+    module Graphics.Pizza.Painting,
+    
+    renderToList
 ) where
+
+-- base
+import Control.Monad
+import Control.Monad.IO.Class
+
+-- linear
+import Linear
+
+-- vector
+import qualified Data.Vector as V 
+
+-- vulkan
+import qualified Vulkan as Vk
 
 -- pizza
 import Graphics.Pizza.Device.Environment
@@ -24,3 +41,40 @@ import Graphics.Pizza.Graphic
 import Graphics.Pizza.Painting
 
 
+-- Facade
+
+-- | Renders graphics into a single list.
+renderToList :: (MonadIO m, Format px) => V2 Int -> Graphics -> m [px]
+renderToList (V2 width height) graphics = do
+    -- Environment
+    environment <- newBasicEnvironment
+    let Environment {..} = environment
+
+    -- Pizzas
+    renderCore <- newRenderCore environment
+    
+    renderer <- newRenderer renderCore Vk.IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
+    renderTarget <- newRenderTarget renderCore renderer width height
+    renderState <- newRenderState renderCore
+    exchange <- newExchangeN renderCore (width * height)
+
+    -- Render image from graphic. It happens on GPU.
+    _ <- renderRenderStateTarget renderCore renderer renderState graphics renderTarget Nothing
+
+    -- Wait image to exchange, to access pixels in CPU.
+    _ <- join $ writeExchangeRenderTarget renderCore exchange renderTarget (Just (renderStateSemaphore renderState))
+    _ <- Vk.waitForFences environmentDevice (V.singleton (exchangeFence exchange)) True maxBound
+
+    -- Copy image pixels from exchange.
+    result <- readExchangeN renderCore exchange (width * height)
+
+    -- Wrap up & clean up
+    freeExchange renderCore exchange
+    freeRenderState renderCore renderState
+    freeRenderTarget renderCore renderTarget
+    freeRenderer renderer
+    freeRenderCore renderCore
+
+    freeEnvironment environment
+
+    pure result
--- pizza/tests/ImageRender.hs
+++ pizza/tests/ImageRender.hs
@@ -23,36 +23,5 @@ import Graphics.Pizza
 
 makeRenderedImage :: Graphics -> IO [V4 Word8]
 makeRenderedImage graphics = do
-
-    -- Environment
-    environment <- newBasicEnvironment
-    let Environment {..} = environment
-
-    -- Pizzas
-    renderCore <- newRenderCore environment
-
-    renderer <- newRenderer renderCore Vk.IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL :: IO (Renderer (VRGBA (UNorm Word8)))
-    renderTarget <- newRenderTarget renderCore renderer 200 200
-    renderState <- newRenderState renderCore
-    exchange <- newExchangeN renderCore (200 * 200)
-
-    -- Render image from graphic. It happens on GPU.
-    _ <- renderRenderStateTarget renderCore renderer renderState graphics renderTarget Nothing
-
-    -- Wait image to exchange, to access pixels in CPU.
-    join $ writeExchangeRenderTarget renderCore exchange renderTarget (Just (renderStateSemaphore renderState))
-    _ <- Vk.waitForFences environmentDevice (V.singleton (exchangeFence exchange)) True maxBound
-
-    -- Copy image pixels from exchange.
-    result <- readExchangeN renderCore exchange (200 * 200)
-
-    -- Wrap up & clean up
-    freeExchange renderCore exchange
-    freeRenderState renderCore renderState
-    freeRenderTarget renderCore renderTarget
-    freeRenderer renderer
-    freeRenderCore renderCore
-
-    freeEnvironment environment
-
+    result <- renderToList (V2 200 200) graphics
     pure (fmap (fmap getUNorm . unVRGBA) result)
